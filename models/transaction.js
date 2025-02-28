fcfd43cbd3c771c8950449c4e955ffc2e1036dcb
Ankit Pandey <ankit221601@gmail.com>
2025-02-28 23:36:24 +0530

    created transaction models

---
+++ models/transaction.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const transactionSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    order: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Order",
+    },
+    paymentId: { type: String, required: true },
+    orderId: { type: String, required: true },
+    status: {
+        type: String,
+        enum: [ "Success", "Failed", "Pending" ],
+        required: true,
+    },
+    amount: { type: Number, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+});
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+export default Transaction;
