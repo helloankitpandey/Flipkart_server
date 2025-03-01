5c05b89c4eecb841944e7a9ee80e662bb4e231cc
Ankit Pandey <ankit221601@gmail.com>
2025-03-01 23:36:38 +0530

    added app.js

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.js";
 import categoryRoutes from "./routes/category.js";
 import productRoutes from "./routes/product.js";
+import orderRoutes from "./routes/order.js";
+import connectDB from "./config/connect.js";
+import { PORT } from "./config/config.js";
+import { buildAdminJS } from "./config/setup.js";
+
 
 
 // config for readablity of all env in our application
@@ -19,15 +24,23 @@ app.use(express.json())
 app.use("/user", userRoutes);
 app.use("/category",categoryRoutes);
 app.use("/product", productRoutes);
+app.use("/order", orderRoutes);
 
 const start = async() => {
     console.log("Radhe Radhe");
     try {
-        app.listen({port: 3000, host: "0.0.0.0" }, (err, addr) => {
+
+        // CONNECTING TO MONGODB
+        await connectDB(process.env.MONGO_URI);
+
+        // 
+        await buildAdminJS(app);
+
+        app.listen({port: PORT , host: "0.0.0.0" }, (err, addr) => {
             if(err){
                 console.log(err);
             } else {
-                console.log("Server started on http://localhost:3000");   
+                console.log(`Server started on http://localhost:${PORT}/admin`);   
             }
         } )
     } catch (error) {
